2b6b8c051b21d4e11fe9be76067b7f0c14661269
RodrigoBRamos <systemas.rodrigo@gmail.com>
2017-10-04 14:04:05 -0300

    subindo Projeti_02

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>..::Compra&Venda Meio do Mundo::..</title>
+    <meta name="author" content="Anderson Cruz,Rodrigo Ramos, Weslley Dutra">
+    <meta name="description" content="Site de vendas...">
+    <link rel="icon" href="img/amapa.png" type="image/x-icon"/>
+    <link rel="shortcut icon" href="img/amapa.png" type="image/x-icon"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/myStyle.css">
+</head>
+<body>
+<main class="container-fluid col bg-primary">
+<!-- inicio do Menu -->  
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <img src="./img/topo/capa1.png" width="300" height="160%" class="d-inline-block align-top img-fluid" alt="">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="./index.html">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="./php.html">Acessórios</a>
+            <a class="nav-item nav-link" href="./contact.html">Automtivos</a>
+            <a class="nav-item nav-link" href="./contact.html">Eletrônicos</a>
+            <a class="nav-item nav-link" href="./contact.html">Eletrôdomesticos</a>
+            <a class="nav-item nav-link" href="./contact.html">Imovéis</a>
+            <a class="nav-item nav-link" href="./contact.html">Cadastro</a>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder=" Pesquisar" aria-label="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisa</button>
+          </form>
+
+          </div>
+            
+        </div>
+
+      </nav>
+      <img src="./img/topo/capa1.png" width="100%" height="100%" class="align-top img-fluid" alt="Compra e Vendo Meio do Mundo">
+      <!-- Fim do Menu --> 
+      <!-- Inicio Topo -->
+      <section id="carousel" class="container-fluid col-10">
+          <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+          <div class="carousel-inner" role="listbox">
+              <div class="carousel-item active">
+                <img class="d-block img-fluid align-middle" src="img/02.jpg" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="img/03.jpg" alt="Second slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="img/01.jpg" alt="Third slide">
+              </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </section>
+      <!-- Fim topo -->  
+</main>
+<script src="js/jquery-3.2.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>

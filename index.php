3de68210d1bdefd9484e06f639ecba166d40532a
RodrigoBRamos <systemas.rodrigo@gmail.com>
2017-10-10 13:13:33 -0300

    Organizando o Progresso

--- index.php
+++ index.php
@@ -5,47 +5,43 @@
 	<title>..::Compra&Venda Meio do Mundo::..</title>
     <meta name="author" content="Anderson Cruz,Rodrigo Ramos, Weslley Dutra">
     <meta name="description" content="Site de vendas...">
+		<meta name="keywords" content="1.Vendas,2 compras, venda e compra, Vender, vende, Comprar" />
     <link rel="icon" href="img/amapa.png" type="image/x-icon"/>
     <link rel="shortcut icon" href="img/amapa.png" type="image/x-icon"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="css/myStyle.css">
-  <link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" type="text/css" media="all" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" media="screen" href="css/index.css">
 </head>
 <body>
-<main class="container-fluid col bg-primary">
-<!-- inicio do Menu -->
-	   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-     <figure>
-        <img src="./img/topo/capa1.png" width="300" height="160%" class="d-inline-block align-top img-fluid" alt="">
-      </figure>
+<main class="container-fluid col bg-primary">	<!-- principal -->
+	<header><!-- inicio do Menu -->
+  	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	     <figure>
+	        <img src="./img/topo/capa.png" width="300px" height="160px" class="d-inline-block align-top img-fluid" alt="">
+	      </figure>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="./index.html">Home <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="./php.html">Acessórios</a>
-            <a class="nav-item nav-link" href="./contact.html">Automtivos</a>
-            <a class="nav-item nav-link" href="./contact.html">Eletrônicos</a>
-            <a class="nav-item nav-link" href="./contact.html">Eletrôdomesticos</a>
-            <a class="nav-item nav-link" href="./contact.html">Imovéis</a>
-            <a class="nav-item nav-link" href="./contact.html">Cadastro</a>
-          <form class="form-inline my-2 my-lg-0 direita">
-            <input class="form-control mr-sm-2" type="text" placeholder=" Pesquisar" aria-label="Search">
-              <button class="btn btn-outline-success my-2 my-sm-0 pesquisa" type="submit">Pesquisa</button>
-          </form>
-
+            <a class="nav-item nav-link active" href="./index.php">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="./pg/acessorios.php">Acessórios</a>
+            <a class="nav-item nav-link" href="./pg/automotivos.php">Automtivos</a>
+            <a class="nav-item nav-link" href="./pg/eletronicos.php">Eletrônicos</a>
+            <a class="nav-item nav-link" href="./pg/eletrodomesticos.php">Eletrôdomesticos</a>
+            <a class="nav-item nav-link" href="./pg/imoveis.php">Imovéis</a>
+            <a class="nav-item nav-link" href="./pg/login.php">Login</a>
+							<!-- botao de pesquisa no menu -->
+							<form class="form-inline my-2 my-lg-0 direita">
+		            <input class="form-control mr-sm-2" type="text" placeholder=" Pesquisar" aria-label="Search">
+		              <button class="btn btn-outline-success my-2 my-sm-0 pesquisa" type="submit">Pesquisa</button>
+		          </form>
           </div>
-
         </div>
-
-      </nav>
-      <figure id="topo">
+      </nav> <!-- Fim do Menu -->
+      <figure id="topo"> <!-- Logo Menu -->
         <img src="./img/topo/capa1.png" width="100%" height="100%" class="align-top img-fluid" alt="Compra e Vendo Meio do Mundo">
       </figure>
-      <!-- Fim do Menu -->
-
       <!-- Inicio Topo -->
       <section id="carousel" class="container-fluid col-10">
           <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
@@ -74,13 +70,17 @@
             <span class="sr-only">Next</span>
           </a>
         </div>
-      </section>
-      <!-- Fim topo -->
+      </section><!-- Fim topo -->
+		</header> <!-- inicio conteudo -->
+		<article>
 
+		</article>	<!-- fim conteudo -->
+		<footer> <!-- inicio rodape -->
 
+		</footer>
 </main>
-<script src="js/jquery-3.2.1.slim.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery-3.2.1.slim.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
